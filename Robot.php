303ee6ebf3fef35882bf0de6c63da1c6e43b86a0
a2824256 <lygg725@163.com>
2018-07-21 16:29:20 +0800

    random user agent

--- Robot.php
+++ Robot.php
@@ -1,11 +1,15 @@
 <?php
+
 namespace autoPushWebsite;
 include_once "Db.php";
+include_once "Helper.php";
 
 use swoole_process;
+
 class Robot
 {
-    public $workerMaxNum = 2;
+    //CPU核心数目
+    public $workerMaxNum = 32;
     public $works = [];
     public $masterPid;
     public $urls = [];
@@ -20,8 +24,8 @@ class Robot
                 'server' => '127.0.0.1',
                 'port' => '3306',
                 'username' => 'root',
-//                'password' => '296b1654c32ceb03',
-                'password' => 'newlife',
+                'password' => '296b1654c32ceb03',
+//                'password' => 'newlife',
                 'database_name' => 'spider',
                 'database_type' => 'mysql',
                 'charset' => 'utf8',
@@ -29,7 +33,7 @@ class Robot
             $this->urls = Db::instance('spider')->select("urls", ["url"]);
             $this->engines = Db::instance('spider')->select("engines", ["engine"]);
             $this->masterPid = posix_getpid();
-            echo "master Pid " . $this->masterPid . PHP_EOL;
+            echo "主线程启动： Pid " . $this->masterPid . PHP_EOL;
             $this->run();
             $this->processWait();
         } catch (\Exception $e) {
@@ -54,10 +58,10 @@ class Robot
                 $this->checkMasterPid($worker);
             }
             $this->getStatus($this->engines[$engineIndex], $engineIndex);
-            echo "engineIndex:" . $engineIndex . PHP_EOL;
+            //echo "engineIndex:" . $engineIndex . PHP_EOL;
         }, false, false);
         $pid = $process->start();
-        echo "worker pid:" . $pid . PHP_EOL;
+        //echo "worker pid:" . $pid . PHP_EOL;
         $this->works[$pid] = $pid;
         return $pid;
     }
@@ -65,18 +69,22 @@ class Robot
     public function getStatus($engine, $engineIndex)
     {
         if ($engineIndex >= count($this->engines)) {
-            echo "engines max" . PHP_EOL;
+//            echo "engines max" . PHP_EOL;
             return;
         }
         foreach ($this->urls as $value) {
             $ch = curl_init($engine["engine"] . $value["url"]);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT_MS, 5000);
+            curl_setopt($ch, CURLOPT_USERAGENT, Helper::randUserAgent("pc"));
             curl_exec($ch);
             $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-//            echo $statusCode . " " . $engine . $value .  PHP_EOL;
+            if ($statusCode != 200) {
+                echo $statusCode . ":" . $engine["engine"] . $value["url"];
+            }
             curl_close($ch);
         }
-        echo $engine["engine"] . " finish" . PHP_EOL;
+        echo $engine["engine"] . " 搜索引擎处理结束" . PHP_EOL;
         return;
     }
 
@@ -97,16 +105,16 @@ class Robot
     public function rebootProcess($ret)
     {
         $pid = $ret['pid'];
-        echo "rb pid:" . $pid . PHP_EOL;
+        //echo "rb pid:" . $pid . PHP_EOL;
         $index = in_array($pid, $this->works);
         if ($index) {
             unset($this->works[$pid]);
             $pid = intval($pid);
             if ($this->engineIndex < count($this->engines)) {
                 $new_pid = $this->createProcess($pid);
-                echo "rebootProcess: {$pid}={$new_pid} Done\n";
+                //echo "rebootProcess: {$pid}={$new_pid} Done\n";
             } else {
-                echo "rb engines max,engineIndex:" . $this->engineIndex . ",engines count:" . count($this->engines) . PHP_EOL;
+                //echo "rb engines max,engineIndex:" . $this->engineIndex . ",engines count:" . count($this->engines) . PHP_EOL;
             }
             return;
         } elseif ($pid == $this->masterPid) {
